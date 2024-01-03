39b3fe2b63c66a5e16f1dff7b9a0913a74efef06
Tom Bouillut <tom@p113.io>
2024-01-03 18:28:23 -0300

    debug build, dev, debug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
 
 // Charge les variables d'environnement depuis le fichier .env
 dotenv.config();
 
-const app = express();
-const port = process.env.SERVER_PORT || 3000;
+const app: Express = express();
+const port: number = parseInt(process.env.SERVER_PORT || '3010', 10);
 
 // Importe les routes depuis le fichier routes.ts
 import { routes } from './routes';
@@ -13,6 +13,6 @@ import { routes } from './routes';
 // Utilise les routes
 app.use('/', routes);
 
-app.listen(port, () => {
+app.listen(port, (): void => {
   console.log(`Serveur écoutant sur le port ${port}.`);
 });
--- backend/src/middleware/auth/auth.ts
+++ backend/src/middleware/auth/auth.ts
@@ -1,23 +1,36 @@
 import { Request, Response } from 'express';
 
-export const register = (req: Request, res: Response) => {
-  // Logique d'enregistrement ici
+export const register = (req: Request, res: Response): void => {
+  /**
+   * Logique d'enregistrement ici
+   *
+   * @param req - The request object
+   * @param res - The response object
+   */
   const { name, password, email } = req.body;
   console.log(name, password, email);
   res.send('Enregistrement effectué avec succès!');
 };
 
-export const login = (req: Request, res: Response) => {
-
-  // Logique de connexion ici
+export const login = (req: Request, res: Response): void => {
+  /**
+   * Logique de connexion ici
+   *
+   * @param req - The request object
+   * @param res - The response object
+   */
   const { email, password } = req.body;
-  // Insérez votre code ici
   console.log(email, password);
   res.send('Connexion réussie!');
 };
 
-export const logout = (req: Request, res: Response) => {
-  // Logique de déconnexion ici
+export const logout = (req: Request, res: Response): void => {
+  /**
+   * Logique de déconnexion ici
+   *
+   * @param req - The request object
+   * @param res - The response object
+   */
   const { email, sessionId } = req.body;
   console.log(email, sessionId);
   res.send('Déconnexion réussie!');
