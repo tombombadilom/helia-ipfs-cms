4a5940e50453eaee3ba81542d1cea92783e100df
Tom Bouillut <tom@p113.io>
2024-01-03 18:29:22 -0300

    rename eslintrc.js in eslintrc.cjs

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Add your ESLint rules here
+  },
+};
