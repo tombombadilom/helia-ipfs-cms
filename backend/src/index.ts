6faad56daa6afd97f79749ec41796a0bd88dce6d
Tom Bouillut <tom@p113.io>
2024-01-05 15:26:18 -0300

    added strong types and loaded .env variable in index.ts 	modified:   backend/access.log 	modified:   backend/error.log 	modified:   backend/src/index.ts 	modified:   backend/src/middleware/auth/auth.ts 	deleted:    backend/src/routes.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,15 +8,16 @@ import * as path from 'path';
 dotenv.config();
 
 const app: Express = express();
-const port: number = parseInt(process.env.SERVER_PORT || '3010', 10);
-
+const protocol: 'http' | 'https' = (process.env.PROTOCOL as string).toLowerCase() as 'http' | 'https';
+const apiUrl = process.env.API_URL;
+const port: number = parseInt(process.env.PORT || '3010', 10);
 let routes: string[] = [];
 
 // Obtient le chemin absolu du répertoire actuel
 const currentDirectory = process.cwd();
 // Construit le chemin absolu pour le répertoire des routes
-const routesDirectoryPath = path.join(currentDirectory, 'routes');
-
+const routesDirectoryPath = path.join(currentDirectory, 'src/routes');
+console.log("routes",routesDirectoryPath);
 // Vérifie l'existence du répertoire des routes
 if (fs.existsSync(routesDirectoryPath)) {
   routes = fs.readdirSync(routesDirectoryPath)
@@ -38,16 +39,19 @@ app.use(morgan('combined', { stream: errorLogStream }));
 const router: Router = express.Router();
 
 app.use(cors());
+app.use(express.json());
 app.use("/", router);
 
 router.get('/:slug', (req: Request, res: Response): void => {
   const slug: string = req.params.slug;
- 
+  console.log("slug", slug);
+
   if (!slug) {
     res.redirect('/routes/home.ts');
   } else {
     const forbiddenCharactersRegex = /[.\^$*+?{}[\]|\(\)]/g;
     const cleanedSlug = slug.replace(forbiddenCharactersRegex, '');
+    console.log("cleanedSlug", cleanedSlug);
 
     if (routes.includes(cleanedSlug)) {
       res.redirect(`/routes/${cleanedSlug}.ts`);
@@ -70,6 +74,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
   res.status(500).send(slug ? slug + ' Internal Server Error' : 'Internal Server Error');
 });
 
-app.listen(port, (): void => {
-  console.log(`Serveur écoutant sur le port ${port}.`);
+app.listen(port, () => {
+  console.log(`Serveur écoutant sur ${protocol}://${apiUrl}:${port}.`);
 });
--- backend/src/middleware/auth/auth.ts
+++ backend/src/middleware/auth/auth.ts
@@ -1,31 +1,45 @@
 import { Request, Response } from 'express';
 
-export const register = (req: Request, res: Response): void => {
-  /**
-   * Logique d'enregistrement ici
-   *
-   * @param req - The request object
-   * @param res - The response object
-   */
+interface RegisterRequestBody {
+  name: string;
+  password: string;
+  email: string;
+}
+
+interface LoginRequestBody {
+  email: string;
+  password: string;
+}
+
+/**
+ * Registers a user with the provided information.
+ *
+ * @param {Request<unknown, unknown, RegisterRequestBody>} req - The request object containing the user's registration data.
+ * @param {Response} res - The response object used to send a success message.
+ * @return {void} - This function does not return a value.
+ */
+export const register = (req: Request<unknown, unknown, RegisterRequestBody>, res: Response): void => {
   const { name, password, email } = req.body;
   console.log(name, password, email);
-  res.send('Enregistrement effectué avec succès!');
+  res.send('Registration successful!');
 };
-export const login = (req: Request, res: Response): void => {
-  /**
-   * Logique de connexion ici
-   *
-   * @param req - The request object
-   * @param res - The response object
-   */
+
+/**
+ * Logs in a user.
+ *
+ * @param {Request<unknown, unknown, LoginRequestBody>} req - the request object containing the login details
+ * @param {Response} res - the response object to send the login result
+ * @return {void} - This function does not return anything
+ */
+export const login = (req: Request<unknown, unknown, LoginRequestBody>, res: Response): void => {
   const { email, password } = req.body;
   console.log(email, password);
-  
+
   // Perform login logic here
-  
+
   // Check if login was successful
   const loginSuccessful = true; // Replace with your login logic
-  
+
   if (loginSuccessful) {
     res.json({ success: true, message: "Login successful", error: null });
   } else {
@@ -33,14 +47,15 @@ export const login = (req: Request, res: Response): void => {
   }
 };
 
-export const logout = (req: Request, res: Response): void => {
-  /**
-   * Logique de déconnexion ici
-   *
-   * @param req - The request object
-   * @param res - The response object
-   */
+/**
+ * Logs out a user by invalidating their session.
+ *
+ * @param {Request<unknown, unknown, { email: string, sessionId: number }>} req - The request object containing the user's email and session ID.
+ * @param {Response} res - The response object.
+ * @return {void} This function does not return anything.
+ */
+export const logout = (req: Request<unknown, unknown, { email: string, sessionId: number }>, res: Response): void => {
   const { email, sessionId } = req.body;
   console.log(email, sessionId);
-  res.send('Déconnexion réussie!');
+  res.send('Logout successful!');
 };
