fd1ff572ac9d37fd54ba0238a0b74626ec0f6543
Tom Bouillut <tom@p113.io>
2024-01-09 04:41:40 -0300

    added option fotr tdc compolation and fixed 404 page

--- backend/src/routes/404.ts
+++ backend/src/routes/404.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
-import { handle404 } from '../page/404';
+import { handle404 } from '../page/404.ts';
 
 /**
  * Handler for 404 Not Found response.
- * 
+ *
  * @param req - The request object of type `Request`.
  * @param res - The response object of type `Response`.
  * @returns A `Promise` that resolves to `void`.
  */
 export const handleNotFoundError = (req: Request, res: Response): void => {
-  const slug: string = req.params.slug; // Retrieve the slug parameter from the request
+  const { slug } = req.params; // Retrieve the slug parameter from the request
   res.json(handle404(slug));
-}
+};
